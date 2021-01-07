2f52e382409021c64a98e9a2ce6f7641b9e130b5
George Cope <G.Cope13@gmail.com>
2021-01-07 11:34:49 +0000

    Created a js file & added initial set of element selector variables

---
+++ CodeQuiz.js
@@ -0,0 +1,15 @@
+// Declare variable to select elements by ID
+
+var start = document.getElementById("start");
+var quiz = document.getElementById("quiz");
+var questions = document.getElementById("questions");
+var qImg = document.getElementById("qImg");
+var choices = document.getElementById("choices");
+var choiceA = document.getElementById("A");
+var choiceB = document.getElementById("B");
+var choiceC = document.getElementById("C");
+var timer = document.getElementById("timer");
+var counter = document.getElementById("counter");
+var timeGauge = document.getElementById("timeGauge");
+var progress = document.getElementById("progress");
+var scoreContainer = docuemnt.getElementById("scoreContainer");
