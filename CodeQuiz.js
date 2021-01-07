b9550956c6dfffc5c967cb0bcb7698898fa9a654
George Cope <G.Cope13@gmail.com>
2021-01-07 11:52:26 +0000

    Added an array with questions, choices & answers. More will be added later

--- CodeQuiz.js
+++ CodeQuiz.js
@@ -13,3 +13,37 @@ var counter = document.getElementById("counter");
 var timeGauge = document.getElementById("timeGauge");
 var progress = document.getElementById("progress");
 var scoreContainer = docuemnt.getElementById("scoreContainer");
+
+// Create an array to store our questions, choices & correct answer
+
+let questions = [
+    {
+        question: "What are the 3 core technologies used in web development?",
+        imgSrc: "images/WebTech.jpg",
+        choiceA: "HTML, CSS & Javascript",
+        choiceB: "HTML, Typescript & jQuery",
+        choiceC: "CSS, Javascript & PHP",
+        correct: "A"
+    }, {
+        question: "What does CSS stand for?",
+        imgSrc: "images/HTML.png",
+        choiceA: "Holy Trinity of Machine Learning",
+        choiceB: "HyperText Markup Language",
+        choiceC: "Human Telepathy Magic Liquid",
+        correct: "B"
+    }, {
+        question: "What does DOM stand for?",
+        imgSrc: "images/DOM.png",
+        choiceA: "Disco Oriented Mechanics",
+        choiceB: "Document Object Model",
+        choiceC: "Delicate Oriental Manuscripts",
+        correct: "B"
+    }, {
+        question: "What kind of loop is pictured?",
+        imgSrc: "images/ForLoop.jpg",
+        choiceA: "A while loop",
+        choiceB: "A for each loop",
+        choiceC: "A for loop",
+        correct: "C"
+    }
+];
