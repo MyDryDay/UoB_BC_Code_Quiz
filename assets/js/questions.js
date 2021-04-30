0f559d3bf3ae5881de6251fa6518f9e11e88b442
George Cope <G.Cope13@gmail.com>
2021-04-30 16:52:33 +0100

    Created an assets folder for js and css, added a questions.js file to hold the array of question objects

---
+++ assets/js/questions.js
@@ -0,0 +1,32 @@
+// Create an array to store our questions, choices & correct answer
+var questions = [
+    {
+        question: "What are the 3 core technologies used in web development?",
+        imgSrc: "images/WebTech.jpg",
+        ansA: "HTML, CSS & Javascript",
+        ansB: "HTML, Typescript & jQuery",
+        ansC: "CSS, Javascript & PHP",
+        correct: "A"
+    }, {
+        question: "What does HTML stand for?",
+        imgSrc: "images/HTML.png",
+        ansA: "Holy Trinity of Machine Learning",
+        ansB: "HyperText Markup Language",
+        ansC: "Human Telepathy Magic Liquid",
+        correct: "B"
+    }, {
+        question: "What does DOM stand for?",
+        imgSrc: "images/DOM.png",
+        ansA: "Disco Oriented Mechanics",
+        ansB: "Document Object Model",
+        ansC: "Delicate Oriental Manuscripts",
+        correct: "B"
+    }, {
+        question: "What kind of loop is pictured?",
+        imgSrc: "images/ForLoop.jpg",
+        ansA: "A while loop",
+        ansB: "A for each loop",
+        ansC: "A for loop",
+        correct: "C"
+    }
+];
