a7c41f54cac0b881befe3051d2c426ce0353f3e1
George Cope <G.Cope13@gmail.com>
2021-05-08 18:16:35 +0100

    Added some new questions and altered the original ones

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -2,31 +2,51 @@
 var questions = [
     {
         question: "What are the 3 core technologies used in web development?",
-        imgSrc: "images/WebTech.jpg",
-        ansA: "HTML, CSS & Javascript",
+        ansA: "HTML, CSS & JavaScript",
         ansB: "HTML, Typescript & jQuery",
-        ansC: "CSS, Javascript & PHP",
+        ansC: "CSS, JavaScript & PHP",
         correct: "ansA"
-    }, {
+    }, 
+    {
         question: "What does HTML stand for?",
-        imgSrc: "images/HTML.png",
         ansA: "Holy Trinity of Machine Learning",
         ansB: "HyperText Markup Language",
         ansC: "Human Telepathy Magic Liquid",
         correct: "ansB"
-    }, {
+    }, 
+    {
         question: "What does DOM stand for?",
-        imgSrc: "images/DOM.png",
         ansA: "Disco Oriented Mechanics",
         ansB: "Document Object Model",
         ansC: "Delicate Oriental Manuscripts",
         correct: "ansB"
-    }, {
-        question: "What kind of loop is pictured?",
-        imgSrc: "images/ForLoop.jpg",
-        ansA: "A while loop",
-        ansB: "A for each loop",
-        ansC: "A for loop",
+    }, 
+    {
+        question: "What does API stand for?",
+        ansA: "Apples, Potatoes & Iceburg Lettuce",
+        ansB: "A Particular Image",
+        ansC: "Application Programming Interface",
         correct: "ansC"
+    },
+    {
+        question: "What is HTML used for?",
+        ansA: "The Structure & Contents of a Webpage",
+        ansB: "The Style of a Webpage",
+        ansC: "To Add Interactivity to Webpages",
+        correct: "ansA"
+    },
+    {
+        question: "What is CSS used for?",
+        ansA: "To Add Interactivity to Webpages",
+        ansB: "The Structure & Contents of a Webpage",
+        ansC: "The Style of a Webpage",
+        correct: "ansC"
+    },
+    {
+        question: "What is JavaScript used for?",
+        ansA: "The Style of a Webpage",
+        ansB: "To Add Interactivity to Webpages",
+        ansC: "The Structure & Contents of a Webpage",
+        correct: "ansB"
     }
 ];
